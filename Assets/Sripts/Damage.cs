028599390e4e09e03d1152a1cc2f81f3b71a228a
rililinx <rililinx@gmail.com>
2017-09-09 23:45:35 +0300

    added damage and health classes

---
+++ Assets/Sripts/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Damage : MonoBehaviour {
+    public enum Type {explosive, kinetic, magic };
+    // Use this for initialization
+    Type dmgType;
+    float dmg;
+    public void Hit(Health H)
+    { }
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Sripts/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public enum ArmorType { stone, steel, energetic };
+    ArmorType armType;
+    float armor;
+    float hp;
+
+    public void GetDmg(float dmg, Damage.Type dmgType)
+    {
+        float newhp = hp - CalculateDmg(dmg, dmgType, armor, armType);
+        if (newhp <= 0)
+            Death();
+        else
+            hp = newhp;
+        
+    }
+    public float CalculateDmg(float dmg, Damage.Type dmgType, float armor, ArmorType armType)
+    {
+        return dmg;
+    }
+    public void Death()
+    { }
+  
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
