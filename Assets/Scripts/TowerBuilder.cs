842fba49272890b824ba1c043aa6de54df68a87d
semensan <semensan@ukr.net>
2017-09-10 21:36:21 +0300

    add primitive tower builder

---
+++ Assets/Scripts/TowerBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    public long money = 5000;
+    public GameObject tower;
+    
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+        const long price = 100;
+        if (money < price)
+            return;
+        RaycastHit hit;
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, 1))
+            return;
+        money -= price;
+        Instantiate(tower, hit.point, Quaternion.identity);
+    }
+}
