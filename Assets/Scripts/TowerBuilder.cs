a002137724efa8415b618e7899ba70d60d3f6a79
semensan <semensan@ukr.net>
2017-09-14 16:34:49 +0300

    build snap to grid

--- Assets/Scripts/TowerBuilder.cs
+++ Assets/Scripts/TowerBuilder.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class TowerBuilder : MonoBehaviour
 {
+    public static readonly float gridSize = .5f;
+
     public long money = 5000; // TODO: move to Faction
     public GameObject tower;
+    public GameObject towerGhost;
+
+    private void Awake()
+    {
+        towerGhost = Instantiate(tower);
+        foreach (MonoBehaviour x in towerGhost.GetComponentsInChildren<MonoBehaviour>())
+            Destroy(x);
+        foreach (Collider x in towerGhost.GetComponentsInChildren<Collider>())
+            Destroy(x);
+        foreach (Renderer x in towerGhost.GetComponentsInChildren<Renderer>())
+            x.material.color = Color.LerpUnclamped(x.material.color, Color.green, .5f);
+        towerGhost.SetActive(false);
+    }
 
     void Update()
     {
-        if (!Input.GetMouseButtonDown(0))
-            return;
-        const long price = 100;
-        if (money < price)
-            return;
         RaycastHit hit;
         if (!Ground.coll.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+        {
+            towerGhost.SetActive(false);
+            return;
+        }
+        hit.point = (hit.point / gridSize).Round() * gridSize;
+        towerGhost.transform.position = hit.point;
+        towerGhost.SetActive(true);
+        if (!Input.GetMouseButtonDown(0))
+            return;
+        const long price = 100; // TODO: move to tower
+        if (money < price)
             return;
         money -= price;
         Instantiate(tower, hit.point, Quaternion.identity);
---
+++ Assets/Scripts/utils/Math.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Math
+{
+    public static Vector3 Round(this Vector3 v)
+    {
+        return new Vector3(Mathf.Round(v.x), Mathf.Round(v.y), Mathf.Round(v.z));
+    }
+}
